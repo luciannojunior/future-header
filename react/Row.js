1d62e04851a3d69b40e805d432f6e6ba74d603d8
Lucas Bebber <lucasbbebber@gmail.com>
2019-03-21 10:28:46 -0300

    Fix Row syntax issues and adjust layout

--- react/Row.js
+++ react/Row.js
@@ -3,22 +3,25 @@ import { Container } from 'vtex.store-components'
 
 const Row = ({ children, fullWidth, sticky }) => {
   const content = (
-    <div className="w-100 flex">
+    <div className="w-100 flex items-center">
       {children}
     </div>
   )
 
   return (
     <div
-      className={`w-100 bg-base top-0 ${sticky ? 'z-9' : '' }`}
+      className={`w-100 bg-base top-0 ${sticky ? 'z-999' : '' }`}
       style={{
         // TODO: use `sticky` class once it's available on render
         position: sticky ? 'sticky' : 'relative',
       }}>
       {fullWidth ? content : (
-        <Container className="w-100">
+        <Container className="w-100 flex">
           {content}
         </Container>
       )}
     </div>
   )
+}
+
+export default Row
